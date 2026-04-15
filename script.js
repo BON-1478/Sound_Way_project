2520faab5fef5171c57eeaaf03e7d746fc379416
Bon-1478 <namebronnaja@gmail.com>
2026-04-16 04:16:31 +0700

    commit befor edit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
 const menu = document.querySelector('#mobile-menu');
 const navList = document.querySelector('.nav-list');
 
-menu.addEventListener('click', function() {
-  // สลับการเพิ่ม/ลบ class 'active'
-  navList.classList.toggle('active');
-  
-  // เสริม: ใส่ Animation ให้ปุ่มสามขีดเปลี่ยนเป็นตัว X (ถ้าต้องการ)
-  menu.classList.toggle('is-active');
-});
+if (menu) {
+    menu.addEventListener('click', function() {
+        // เปิด-ปิดเมนู
+        navList.classList.toggle('active');
+        
+        // ถ้าคุณมี CSS สำหรับแปลงร่างปุ่มสามขีดเป็น X
+        menu.classList.toggle('is-active');
+    });
+}
